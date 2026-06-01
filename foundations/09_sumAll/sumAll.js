018c19c1057ef966dffa04554c21ad417c7165a3
RL <renalow37@gmail.com>
2026-06-02 03:18:33 +0800

    Nothing special to commit here. Completed exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
