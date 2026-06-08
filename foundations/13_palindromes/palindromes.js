8026fbdbb79d56c014ab443c49a42961e732de00
RL <renalow37@gmail.com>
2026-06-08 20:57:17 +0800

    Completed, forgot the way join worked

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    let b = a.split("")
+    b.reverse()
+    b = b.join("")
 
+    if (b === a){
+        return true
+    }
+    return false
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
