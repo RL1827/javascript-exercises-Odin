fe3355bcdb7cd7503fab5fb7338ef2dc20ab503d
RL <renalow37@gmail.com>
2026-06-08 21:06:42 +0800

    Finished, error with the loop ending

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    first = 1
+    second = 1
+    third = 2
+    if (a == 1){
+        return first
+    } else if (a == 2){
+        return second
+    } else if (a == 3){
+        return third
+    }
+    for (let i = 3; i<a; i++){
+        let temp = 0
+        temp = third
+        third = third + second
+        first = second
+        second = temp
+    }
+    return third
 };
 
 // Do not edit below this line
